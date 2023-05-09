bf8213e6383c20e62468cf2dd275490293a7c5ae
mischa <125484349+mischa-ai@users.noreply.github.com>
2023-05-10 00:50:52 +0200

    remove powermanager service does not work

--- install-tools.sh
+++ install-tools.sh
@@ -5,14 +5,6 @@ sudo python3 -m pip install --upgrade pip
 # install jtop
 sudo python3 -m pip install -U jetson-stats
 
-# install the wifi power manager disable service
-sudo cp disable-wifi-power-management.service /etc/systemd/system/
-sudo chmod 664 /etc/systemd/system/disable-wifi-power-management.service
-sudo chown root:root /etc/systemd/system/disable-wifi-power-management.service
-sudo systemctl enable disable-wifi-power-management.service
-sudo systemctl start disable-wifi-power-management.service
-# systemctl status disable-wifi-power-management
-
 # install jetson fan control
 cd ~
 git clone https://github.com/Pyrestone/jetson-fan-ctl.git
